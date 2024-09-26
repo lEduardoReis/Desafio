e982d40b614fbe5ddc450527bd796a87435ca2a1
Eduardo Reis <139571244+lEduardoReis@users.noreply.github.com>
2024-09-26 16:13:14 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,25 @@
+# Variáveis de entrada
+nome_heroi = input()  # Exemplo de nome
+xp_heroi = int(input())       # Exemplo de XP
+
+# Estrutura de decisão para classificar o herói
+if xp_heroi < 1000:
+    nivel = "Ferro"
+elif 1001 <= xp_heroi <= 2000:
+    nivel = "Bronze"
+elif 2001 <= xp_heroi <= 5000:
+    nivel = "Prata"
+elif 5001 <= xp_heroi <= 7000:
+    nivel = "Ouro"
+elif 7001 <= xp_heroi <= 8000:
+    nivel = "Platina"
+elif 8001 <= xp_heroi <= 9000:
+    nivel = "Ascendente"
+elif 9001 <= xp_heroi <= 10000:
+    nivel = "Imortal"
+else:
+    nivel = "Radiante"
+
+# Saída final
+mensagem = f"O Herói de nome {nome_heroi} está no nível de {nivel}"
+print(mensagem)
